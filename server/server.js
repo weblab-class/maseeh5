3c8854f2f57af4ecfdb06680c0ee6707a3b61518
jrdegreeff <jrdegreeff@gmail.com>
2020-01-17 13:51:43 -0500

    move secrets to .env

--- server/server.js
+++ server/server.js
@@ -36,9 +36,9 @@ const socket = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "FILL ME IN";
+const mongoConnectionURL = process.env.ATLAS_SRV;
 // TODO change database name to the name you chose
-const databaseName = "FILL ME IN";
+const databaseName = foodforthought;
 
 // connect to mongodb
 mongoose
@@ -60,7 +60,7 @@ app.use(express.json());
 // set up a session, which will persist login data across requests
 app.use(
   session({
-    secret: "session-secret",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
   })
