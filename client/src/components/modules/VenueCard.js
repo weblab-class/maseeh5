908f3ca319ae6af76f36170ae13ce790c3f1b101
jrdegreeff <jrdegreeff@gmail.com>
2020-01-21 15:30:18 -0500

    touch up home page

--- client/src/components/modules/VenueCard.js
+++ client/src/components/modules/VenueCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "@reach/router";
 import Rating from "./Rating";
 
 import "./VenueCard.css";
@@ -13,22 +14,17 @@ import "../../utilities.css";
 class VenueCard extends Component {
   constructor(props) {
     super(props);
-    this.state = {rating: 3};
   }
-
-  componentDidMount() {}
-
-  test = (rating) => {
-    this.setState({rating: rating});
-  };
 
   render() {
     return (
       <>
         <div className="VenueCard-container u-textCenter">
-          <div className="u-bold">{this.props.venue.name}</div>
-          <Rating rating={this.props.venue.rating} />
-          <button className="VenueCard-button u-pointer">See More</button>
+          <div className="VenueCard-title u-bold">{this.props.venue.name}</div>
+          <div className="VenueCard-ratingBox">Average Rating: <Rating rating={this.props.venue.rating} /></div>
+          <Link to={`/feed/${this.props.venue._id}`}>
+            <button className="VenueCard-button u-pointer">See More</button>
+          </Link>
         </div>
       </>
     );
