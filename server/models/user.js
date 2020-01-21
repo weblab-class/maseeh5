c559b28777611b1284e525c22b000aa927b3a4eb
jrdegreeff <jrdegreeff@gmail.com>
2020-01-21 16:24:59 -0500

    add pictureurl to user model

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: String,
   googleid: String,
+  pictureurl: String,
 });
 
 module.exports = mongoose.model("user", UserSchema);
