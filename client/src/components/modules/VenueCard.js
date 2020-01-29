41faa4febb5c4089647caf623ff3fd04f95b33f4
Katherine Liu <katherine.x.liu@gmail.com>
2020-01-29 00:55:41 -0500

    disable closed venues

--- client/src/components/modules/VenueCard.js
+++ client/src/components/modules/VenueCard.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { get } from "../../utilities";
+import { Link } from "@reach/router";
 import Rating from "./Rating";
 
 import "./VenueCard.css";
@@ -13,19 +15,32 @@ import "../../utilities.css";
 class VenueCard extends Component {
   constructor(props) {
     super(props);
+    this.state = { active: true };
+  }
+
+  componentDidMount() {
+    get("/api/reviews").then((reviews) => {
+      this.setState({ active: reviews.length > 0 });
+    });
   }
 
   componentDidMount() {}
 
   render() {
     return (
-      <>
-        <div className="VenueCard-container u-textCenter">
-          <div className="u-bold">{this.props.venue.name}</div>
-          <Rating rating={this.props.venue.rating} />
-          <button className="VenueCard-button u-pointer">See More</button>
+      <div className="VenueCard-container u-textCenter">
+        <div className="VenueCard-title u-bold">{this.props.venue.name}</div>
+        <div className="VenueCard-ratingBox">
+          Average Rating: <Rating rating={this.props.venue.rating} className="VenueCard-rating" />
         </div>
-      </>
+        {this.state.active ? (
+          <Link to={`/feed/${this.props.venue._id}`}>
+            <button className="VenueCard-buttonActive u-pointer">See More</button>
+          </Link>
+        ) : (
+          <button className="VenueCard-buttonInactive">Closed</button>
+        )}
+      </div>
     );
   }
 }
